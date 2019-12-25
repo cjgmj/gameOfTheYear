5f45b9e6b95570faae1fe6552db534680902f885
cjgmj <cjgmj@hotmail.com>
2019-12-25 12:42:36 +0100

    Credenciales para trabajar local y remotamente

--- goty-backend/functions/src/index.ts
+++ goty-backend/functions/src/index.ts
@@ -1,4 +1,14 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://firestore-grafica-64182.firebaseio.com"
+});
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
